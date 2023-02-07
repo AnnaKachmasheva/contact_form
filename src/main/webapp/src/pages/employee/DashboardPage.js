dbfaea9280dc5d777cd2b9212ac5e23de50d6c55
AnnaKachmasheva <annakachmasheva@gmail.com>
2023-02-07 21:30:15 +0100

    added dashboard

--- src/main/webapp/src/pages/employee/DashboardPage.js
+++ src/main/webapp/src/pages/employee/DashboardPage.js
@@ -1,7 +1,26 @@
-function Dashboard () {
+import styles from './Dashboard.module.scss'
+
+
+function Dashboard() {
 
     return (
-        <div>
+        <div className={styles.board}>
+
+            <a className={styles.employees}
+               href="http://localhost:3000/employee">count employee</a>
+
+            <a className={styles.newemployee}
+               href="http://localhost:3000/employee">add employee</a>
+
+
+            <a className={styles.requests}
+               href="http://localhost:3000/contactus">count requests</a>
+
+            <a className={styles.newerequests}
+               href="http://localhost:3000/employee">add request</a>
+
+            <a className={styles.documentation}
+               href="http://localhost:3000/documentation">documentation</a>
 
         </div>
     )
